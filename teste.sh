770d924ec7fc666f914ba1dbc13f4e00e44292cf
Levi Barroso Menezes <levibarrosomenezes@gmail.com>
2019-03-08 19:29:39 -0300

    Update teste.sh

--- teste.sh
+++ teste.sh
@@ -124,106 +124,107 @@ sleep 1
 	debconf-show krb5-config &>> $LOG
 	apt -y install krb5-user krb5-config &>> $LOG
 	mv -v /etc/krb5.conf /etc/krb5.conf.bkp &>> $LOG
+	sleep 3
 	#
 	# Construindo aquivo de configuração do KERBEROS:
-	echo "[libdefaults]" >> /etc/krb5.conf &>> $LOG
+	echo "[libdefaults]" >> /etc/krb5.conf
 	echo "	# Realm padrão" >> /etc/krb5.conf
-	echo "	default_realm = $REINO" >> /etc/krb5.conf &>> $LOG
+	echo "	default_realm = $REINO" >> /etc/krb5.conf
 	echo " " >> /etc/krb5.conf
 	#
 	echo "# Opções utilizadas pela SAMBA4" >> /etc/krb5.conf
-	echo "	dns_lookup_realm = false" >> /etc/krb5.conf &>> $LOG
-	echo "	dns_lookup_kdc = true" >> /etc/krb5.conf &>> $LOG
+	echo "	dns_lookup_realm = false" >> /etc/krb5.conf
+	echo "	dns_lookup_kdc = true" >> /etc/krb5.conf
 	echo " " >> /etc/krb5.conf
 	#
 	echo "# Confguração padrão do Kerneros" >> /etc/krb5.conf
-	echo "	krb4_config = /etc/krb.conf" >> /etc/krb5.conf &>> $LOG
-	echo "	krb4_realms = /etc/krb.realms" >> /etc/krb5.conf &>> $LOG
-	echo "	kdc_timesync = 1" >> /etc/krb5.conf &>> $LOG
-	echo "	ccache_type = 4" >> /etc/krb5.conf &>> $LOG
-	echo "	forwardable = true" >> /etc/krb5.conf &>> $LOG
-	echo "	proxiable = true" >> /etc/krb5.conf &>> $LOG
-	echo "	v4_instance_resolve = false" >> /etc/krb5.conf &>> $LOG
+	echo "	krb4_config = /etc/krb.conf" >> /etc/krb5.conf
+	echo "	krb4_realms = /etc/krb.realms" >> /etc/krb5.conf
+	echo "	kdc_timesync = 1" >> /etc/krb5.conf
+	echo "	ccache_type = 4" >> /etc/krb5.conf
+	echo "	forwardable = true" >> /etc/krb5.conf
+	echo "	proxiable = true" >> /etc/krb5.conf
+	echo "	v4_instance_resolve = false" >> /etc/krb5.conf
 	echo "	v4_name_convert = {" >> /etc/krb5.conf
 	echo "		host = {" >> /etc/krb5.conf
-	echo "			rcmd = host" >> /etc/krb5.conf &>> $LOG
-	echo "			ftp = ftp" >> /etc/krb5.conf &>> $LOG
+	echo "			rcmd = host" >> /etc/krb5.conf
+	echo "			ftp = ftp" >> /etc/krb5.conf
 	echo "		}" >> /etc/krb5.conf
 	echo "		plain = {" >> /etc/krb5.conf
-	echo "			something = something-else" >> /etc/krb5.conf &>> $LOG
+	echo "			something = something-else" >> /etc/krb5.conf
 	echo "		}" >> /etc/krb5.conf
 	echo "	}" >> /etc/krb5.conf
-	echo "	fcc-mit-ticketflags = true" >> /etc/krb5.conf &>> $LOG
+	echo "	fcc-mit-ticketflags = true" >> /etc/krb5.conf
 	echo " " >> /etc/krb5.conf
 	#
 	echo "# Reino padrão" >> /etc/krb5.conf
-	echo "[realms]" >> /etc/krb5.conf &>> $LOG
+	echo "[realms]" >> /etc/krb5.conf
 	echo "	$REINO = {" >> /etc/krb5.conf
 	echo "		# Servidor de geração de KDC" >> /etc/krb5.conf
-	echo "		kdc = $FQDN" >> /etc/krb5.conf &>> $LOG
+	echo "		kdc = $FQDN" >> /etc/krb5.conf
 	echo "		#" >> /etc/krb5.conf
 	echo "		# Servidor de Administração do KDC" >> /etc/krb5.conf
-	echo "		admin_server = $FQDN" >> /etc/krb5.conf &>> $LOG
+	echo "		admin_server = $FQDN" >> /etc/krb5.conf
 	echo "		#" >> /etc/krb5.conf
 	echo "		# Domínio padrão" >> /etc/krb5.conf
-	echo "		default_domain = $DOMINIO" >> /etc/krb5.conf &>> $LOG
+	echo "		default_domain = $DOMINIO" >> /etc/krb5.conf
 	echo "	}" >> /etc/krb5.conf
 	echo " " >> /etc/krb5.conf
 	#
 	echo "# Domínio Realm" >> /etc/krb5.conf
-	echo "[domain_realm]" >> /etc/krb5.conf &>> $LOG
-	echo "	.$DOMINIO = $REINO" >> /etc/krb5.conf &>> $LOG
-	echo "	$DOMINIO = $REINO" >> /etc/krb5.conf &>> $LOG
+	echo "[domain_realm]" >> /etc/krb5.conf
+	echo "	.$DOMINIO = $REINO" >> /etc/krb5.conf
+	echo "	$DOMINIO = $REINO" >> /etc/krb5.conf
 	echo " " >> /etc/krb5.conf
 	#
 	echo "# Geração do Tickets" >> /etc/krb5.conf
-	echo "[login]" >> /etc/krb5.conf &>> $LOG
-	echo "	krb4_convert = true" >> /etc/krb5.conf &>> $LOG
-	echo "	krb4_get_tickets = false" >> /etc/krb5.conf &>> $LOG
+	echo "[login]" >> /etc/krb5.conf
+	echo "	krb4_convert = true" >> /etc/krb5.conf
+	echo "	krb4_get_tickets = false" >> /etc/krb5.conf
 	echo " " >> /etc/krb5.conf
 	#
 	echo "# Log dos tickets do Kerberos" >> /etc/krb5.conf
-	echo "[logging] " >> /etc/krb5.conf &>> $LOG
-	echo "  default = FILE:/var/log/krb5libs.log " >> /etc/krb5.conf &>> $LOG
-	echo "  kdc = FILE:/var/krb5/krb5kdc.log " >> /etc/krb5.conf &>> $LOG
-	echo "  admin_server = FILE:/var/log/krb5admin.log" >> /etc/krb5.conf &>> $LOG
+	echo "[logging] " >> /etc/krb5.conf
+	echo "  default = FILE:/var/log/krb5libs.log " >> /etc/krb5.conf
+	echo "  kdc = FILE:/var/krb5/krb5kdc.log " >> /etc/krb5.conf
+	echo "  admin_server = FILE:/var/log/krb5admin.log" >> /etc/krb5.conf
 	echo -e "Kerberos ...............................................[ OK ]"
 sleep 1
 #
 #Configurar NTP:
 	#echo -e "Configurando NTP ..."	
-	echo "0.0" > /var/lib/ntp/ntp.drift &>> $LOG
+	echo "0.0" > /var/lib/ntp/ntp.drift
 	chown -v ntp.ntp /var/lib/ntp/ntp.drift &>> $LOG
 	mv -v /etc/ntp.conf /etc/ntp.conf.bkp &>> $LOG
 	#
 	# Construindo aquivo de configuração do NTP:
-	echo "driftfile /var/lib/ntp/ntp.drift" >> /etc/ntp.conf &>> $LOG
+	echo "driftfile /var/lib/ntp/ntp.drift" >> /etc/ntp.conf
 	#
 	echo "#Estatísticas do ntp que permitem verificar o histórico" >> /etc/ntp.conf
-	echo "statsdir /var/log/ntpstats/" >> /etc/ntp.conf &>> $LOG
-	echo "statistics loopstats peerstats clockstats" >> /etc/ntp.conf &>> $LOG
-	echo "filegen loopstats file loopstats type day enable" >> /etc/ntp.conf &>> $LOG
-	echo "filegen peerstats file peerstats type day enable" >> /etc/ntp.conf &>> $LOG
-	echo "filegen clockstats file clockstats type day enable" >> /etc/ntp.conf &>> $LOG
+	echo "statsdir /var/log/ntpstats/" >> /etc/ntp.conf
+	echo "statistics loopstats peerstats clockstats" >> /etc/ntp.conf
+	echo "filegen loopstats file loopstats type day enable" >> /etc/ntp.conf
+	echo "filegen peerstats file peerstats type day enable" >> /etc/ntp.conf
+	echo "filegen clockstats file clockstats type day enable" >> /etc/ntp.conf
 	echo " " >> /etc/ntp.conf
 	#
 	echo "#Servidores publicos ntp.br" >> /etc/ntp.conf
-	echo "server a.st1.ntp.br iburst" >> /etc/ntp.conf &>> $LOG
-	echo "server b.st1.ntp.br iburst" >> /etc/ntp.conf &>> $LOG
-	echo "server c.st1.ntp.br iburst" >> /etc/ntp.conf &>> $LOG
-	echo "server d.st1.ntp.br iburst" >> /etc/ntp.conf &>> $LOG
-	echo "server gps.ntp.br iburst" >> /etc/ntp.conf &>> $LOG
-	echo "server a.ntp.br iburst" >> /etc/ntp.conf &>> $LOG
-	echo "server b.ntp.br iburst" >> /etc/ntp.conf &>> $LOG
-	echo "server c.ntp.br iburst" >> /etc/ntp.conf &>> $LOG
-	echo " " >> /etc/ntp.conf &>> $LOG
+	echo "server a.st1.ntp.br iburst" >> /etc/ntp.conf
+	echo "server b.st1.ntp.br iburst" >> /etc/ntp.conf
+	echo "server c.st1.ntp.br iburst" >> /etc/ntp.conf
+	echo "server d.st1.ntp.br iburst" >> /etc/ntp.conf
+	echo "server gps.ntp.br iburst" >> /etc/ntp.conf
+	echo "server a.ntp.br iburst" >> /etc/ntp.conf
+	echo "server b.ntp.br iburst" >> /etc/ntp.conf
+	echo "server c.ntp.br iburst" >> /etc/ntp.conf
+	echo " " >> /etc/ntp.conf
 	#
 	echo "#Configuraçõess de restrição de acesso" >> /etc/ntp.conf
-	echo "restrict 127.0.0.1" >> /etc/ntp.conf &>> $LOG
-	echo "restrict 127.0.1.1" >> /etc/ntp.conf &>> $LOG
-	echo "restrict ::1" >> /etc/ntp.conf &>> $LOG
-	echo "restrict default kod notrap nomodify nopeer noquery" >> /etc/ntp.conf &>> $LOG
-	echo "restrict -6 default kod notrap nomodify nopeer noquery" >> /etc/ntp.conf &>> $LOG
+	echo "restrict 127.0.0.1" >> /etc/ntp.conf
+	echo "restrict 127.0.1.1" >> /etc/ntp.conf
+	echo "restrict ::1" >> /etc/ntp.conf
+	echo "restrict default kod notrap nomodify nopeer noquery" >> /etc/ntp.conf
+	echo "restrict -6 default kod notrap nomodify nopeer noquery" >> /etc/ntp.conf
 	#
 	systemctl stop ntp.service &>> $LOG
 	timedatectl set-timezone "America/Fortaleza" &>> $LOG
@@ -253,18 +254,18 @@ sleep 1
 	mv -v /etc/hosts /etc/hosts.bkp &>> $LOG
 	#
 	# Construindo aquivo de configuração do HOSTS:
-	echo "#IPv4" >> /etc/hostname
-	echo "$IP		$FQDN		$NOME" >> /etc/hostname &>> $LOG
-	echo "127.0.0.1		localhost.localdomain		localhost" >> /etc/hostname &>> $LOG
-	echo "" >> /etc/hostname
+	echo "#IPv4" >> /etc/hosts
+	echo "$IP		$FQDN		$NOME" >> /etc/hosts
+	echo "127.0.0.1		localhost.localdomain		localhost" >> /etc/hosts
+	echo "" >> /etc/hosts
 	#
-	echo "#IPv6" >> /etc/hostname
-	echo "::1			localhost6.localdomain6		localhost6" >> /etc/hostname &>> $LOG
-	echo "::1			localhost ip6-localhost ip6-loopback" >> /etc/hostname &>> $LOG
-	echo "fe00::0		ip6-localnet" >> /etc/hostname &>> $LOG
-	echo "ff02::1		ip6-allnodes" >> /etc/hostname &>> $LOG
-	echo "ff02::2		ip6-allrouters" >> /etc/hostname &>> $LOG
-	echo "ff02::3		ip6-allhosts" >> /etc/hostname &>> $LOG
+	echo "#IPv6" >> /etc/hosts
+	echo "::1			localhost6.localdomain6		localhost6" >> /etc/hosts
+	echo "::1			localhost ip6-localhost ip6-loopback" >> /etc/hosts
+	echo "fe00::0		ip6-localnet" >> /etc/hosts
+	echo "ff02::1		ip6-allnodes" >> /etc/hosts
+	echo "ff02::2		ip6-allrouters" >> /etc/hosts
+	echo "ff02::3		ip6-allhosts" >> /etc/hosts
 	#
 	echo -e "Resolução local de nomes (hosts) .......................[ OK ]"
 sleep 1
@@ -274,26 +275,26 @@ sleep 1
 	#
 	# Construindo aquivo de configuração do HOSTS:
 	echo "# Habilitar os recursos de files (arquivos) e winbind (integração) SAMBA+GNU/Linux" >> /etc/nsswitch.conf
-	echo "passwd:         files compat systemd winbind" >> /etc/nsswitch.conf &>> $LOG
-	echo "group:          files compat systemd winbind" >> /etc/nsswitch.conf &>> $LOG
-	echo "shadow:         files compat systemd winbind" >> /etc/nsswitch.conf &>> $LOG
-	echo "gshadow:        files" >> /etc/nsswitch.conf &>> $LOG
+	echo "passwd:         files compat systemd winbind" >> /etc/nsswitch.conf
+	echo "group:          files compat systemd winbind" >> /etc/nsswitch.conf
+	echo "shadow:         files compat systemd winbind" >> /etc/nsswitch.conf
+	echo "gshadow:        files" >> /etc/nsswitch.conf
 	echo "" >> /etc/nsswitch.conf
 	#
 	echo "# Configuração de resolução de nomes" >> /etc/nsswitch.conf
-	echo "# Habilitar o recursos de dns depois de files (arquivo hosts)" >> /etc/nsswitch.conf &>> $LOG
-	echo "hosts:          files dns mdns4_minimal [NOTFOUND=return]" >> /etc/nsswitch.conf &>> $LOG
-	echo "networks:       files" >> /etc/nsswitch.conf &>> $LOG
+	echo "# Habilitar o recursos de dns depois de files (arquivo hosts)" >> /etc/nsswitch.conf
+	echo "hosts:          files dns mdns4_minimal [NOTFOUND=return]" >> /etc/nsswitch.conf
+	echo "networks:       files" >> /etc/nsswitch.conf
 	echo "" >> /etc/nsswitch.conf
 	#
 	echo "#Configurações padrão." >> /etc/nsswitch.conf
-	echo "protocols:      db files" >> /etc/nsswitch.conf &>> $LOG
-	echo "services:       db files" >> /etc/nsswitch.conf &>> $LOG
-	echo "ethers:         db files" >> /etc/nsswitch.conf &>> $LOG
-	echo "rpc:            db files" >> /etc/nsswitch.conf &>> $LOG
-	echo "netgroup:       nis" >> /etc/nsswitch.conf &>> $LOG
+	echo "protocols:      db files" >> /etc/nsswitch.conf
+	echo "services:       db files" >> /etc/nsswitch.conf
+	echo "ethers:         db files" >> /etc/nsswitch.conf
+	echo "rpc:            db files" >> /etc/nsswitch.conf
+	echo "netgroup:       nis" >> /etc/nsswitch.conf
 	#
-	echo -e "Ponte NS ...............................................[ OK ]"
+	echo -e "Ponte NS (nsswitch) ....................................[ OK ]"
 sleep 1
 #
 #Instalar SAMBA4:
@@ -303,7 +304,6 @@ sleep 1
 sleep 1
 #
 #Configurar interfaces de rede:
-	sleep 3
 	mv /etc/netplan/50-cloud-init.yaml /etc/netplan/50-cloud-init.yaml.bkp
 	#
 	# Construindo aquivo de configuração do NETPLAN:
@@ -346,3 +346,13 @@ sleep 1
 sleep 1
 read
 exit 1
+
+#
+#Variáveis do script 2	
+#HORAFINAL=`date +%T`
+#HORAINICIAL01=$(date -u -d "$HORAINICIAL" +"%s")
+#HORAFINAL01=$(date -u -d "$HORAFINAL" +"%s")
+#TEMPO=`date -u -d "0 $HORAFINAL01 sec - $HORAINICIAL01 sec" +"%H:%M:%S"`
+#
+#echo -e "Tempo de execução $0: $TEMPO"
+#echo -e "Fim do script $0 em: `date +%d/%m/%Y-"("%H:%M")"`\n" &>> $LOG
