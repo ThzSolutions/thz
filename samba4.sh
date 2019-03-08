d9ef9df01622cf91d908274b08960de2396332a4
Levi Barroso Menezes <levibarrosomenezes@gmail.com>
2019-03-08 15:16:59 -0300

    Update samba4.sh

--- samba4.sh
+++ samba4.sh
@@ -353,4 +353,5 @@ TEMPO=`date -u -d "0 $HORAFINAL01 sec - $HORAINICIAL01 sec" +"%H:%M:%S"`
 #
 echo -e "Tempo de execução $0: $TEMPO"
 echo -e "Fim do script $0 em: `date +%d/%m/%Y-"("%H:%M")"`\n" &>> $LOG
+read
 exit 1
