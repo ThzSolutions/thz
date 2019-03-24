b485180f7244b9a867b8bb77956fc9ec765cafb3
Levi Barroso Menezes <levibarrosomenezes@gmail.com>
2019-03-23 22:41:55 -0300

    Update ntp.sh

--- ntp.sh
+++ ntp.sh
@@ -6,9 +6,6 @@
 # Kernel Linux 4.15.x
 #
 #Variáveis do NTP
-NTPINTRA0=""
-NTPINTRA1=""
-NTPINTRA2=""
 ZONA="America/Fortaleza"
 #
 # Exportando o recurso de Noninteractive:
@@ -22,42 +19,41 @@ export DEBIAN_FRONTEND="noninteractive"
 	mv -v /etc/ntp.conf /etc/ntp.conf.bkp &>> $LOG
 	#
 	# Construindo aquivo de configuração do NTP:
-	printf "driftfile /var/lib/ntp/ntp.drift
-			#Estatísticas do ntp que permitem verificar o histórico
-			statsdir /var/log/ntpstats/
-			statistics loopstats peerstats clockstats
-			filegen loopstats file loopstats type day enable
-			filegen peerstats file peerstats type day enable
-			filegen clockstats file clockstats type day enable
+	printf "
+driftfile /var/lib/ntp/ntp.drift
+
+#Estatísticas do ntp que permitem verificar o histórico
+statsdir /var/log/ntpstats/
+statistics loopstats peerstats clockstats
+filegen loopstats file loopstats type day enable
+filegen peerstats file peerstats type day enable
+filegen clockstats file clockstats type day enable
+
+#Servidores publicos ntp.br
+server a.st1.ntp.br iburst
+server b.st1.ntp.br iburst
+server c.st1.ntp.br iburst
+server d.st1.ntp.br iburst
+server gps.ntp.br iburst
+server a.ntp.br iburst
+server b.ntp.br iburst
+server c.ntp.br iburst
 			
-			#Servidores publicos ntp.br
-			server a.st1.ntp.br iburst
-			server b.st1.ntp.br iburst
-			server c.st1.ntp.br iburst
-			server d.st1.ntp.br iburst
-			server gps.ntp.br iburst
-			server a.ntp.br iburst
-			server b.ntp.br iburst
-			server c.ntp.br iburst
-			
-			#Servidores internos
-			server @NTPINTRA0 iburst
-			server @NTPINTRA1 iburst
-			server @NTPINTRA2 iburst
-			
-			#Configurações de restrição de acesso
-			restrict 127.0.0.1
-			restrict 127.0.1.1
-			restrict ::1
-			restrict default kod notrap nomodify nopeer noquery
-			restrict -6 default kod notrap nomodify nopeer noquery
+#Configurações de restrição de acesso
+restrict 127.0.0.1
+restrict 127.0.1.1
+restrict ::1
+restrict default kod notrap nomodify nopeer noquery
+restrict -6 default kod notrap nomodify nopeer noquery
 	" > /etc/ntp.conf
-	systemctl stop ntp.service &>> $LOG
-	timedatectl set-timezone "$ZONA" &>> $LOG
-	ntpdate -dquv $NTP &>> $LOG
-	systemctl start ntp.service &>> $LOG
-	ntpq -pn &>> $LOG
-	hwclock --systohc &>> $LOG
+	systemctl stop ntp.service
+	timedatectl set-timezone "$ZONA"
+	ntpdate -dquv $NTP
+	update-rc.d ntp.service defaults
+	systemctl enable ntp.service
+	systemctl start ntp.service
+	ntpq -pn
+	hwclock --systohc
 	#echo "Data/Hora de hardware: `hwclock`\n"
 	#echo "Data/Hora de software: `date`\n"
 sleep 1
